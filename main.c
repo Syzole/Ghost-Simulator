40415d9a0814cdf5bb89cca1fc654760d2451f3e
Syzole <championarjun2004@gmail.com>
2023-11-25 19:22:11 -0500

    Adding base files

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "defs.h"
+
+int main()
+{
+    // Initialize the random number generator
+    srand(time(NULL));
+
+    // Create the house: You may change this, but it's here for demonstration purposes
+    // Note: This code will not compile until you have implemented the house functions and structures
+    HouseType house;
+    initHouse(&house);
+    populateRooms(&house);
+
+    return 0;
+}
+
