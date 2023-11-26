cb850a2703434fda38e6b4bb64a54bf7ea7438dd
Syzole <championarjun2004@gmail.com>
2023-11-26 01:23:09 -0500

    fixed create room

--- house.c
+++ house.c
@@ -56,19 +56,10 @@ void populateRooms(HouseType* house) {
 }
 
 Room* createRoom(char* name) {
-    // Allocate memory for the room
     Room* room = (Room*)malloc(sizeof(Room));
-
-    // Check for allocation failure
-    if (room == NULL) {
-        // Handle allocation failure, maybe print an error message or return NULL
-        return NULL;
-    }
-
-    // Initialize room members
-    // For example:
     strcpy(room->name, name);
     room->ghost = NULL;    
+    return room;
 }
 
 void initHouse(){
