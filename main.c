15a3fe889101a69955ed239952a1715177bf52bf
Syzole <championarjun2004@gmail.com>
2023-11-26 23:20:13 -0500

    Adjusted Main

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include "defs.h"
 
 int main()
-{
+{ 
     // Initialize the random number generator
     srand(time(NULL));
 
     // Create the house: You may change this, but it's here for demonstration purposes
     // Note: This code will not compile until you have implemented the house functions and structures
     HouseType house;
+    Ghost ghost;
+
+    initGhost(&ghost);
     initHouse(&house);
     populateRooms(&house);
-    //initGhost
+    startHunt(&house, &ghost);
 
     return 0;
 }
