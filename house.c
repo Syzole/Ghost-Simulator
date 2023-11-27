26c3ed547f0ef78a18e4fc18bd7b29df14e51834
llinhir <jessicaglhill@gmail.com>
2023-11-26 21:07:22 -0500

    organized house functions a

--- house.c
+++ house.c
@@ -1,5 +1,101 @@
 #include "defs.h"
 
+void initGhost(Ghost* ghost) {
+    ghost->ghostType = randomGhost();
+    ghost->boredom = 0;
+    ghost->room = NULL;
+}
+void initHunter(Hunter* hunter, House* house, int numHunt) {
+    printf("This is hunter #%d, what should his name be",numHunt+1);
+    scanf("%s", hunter->name);
+    hunter->roomIn = house->totalRoomList.head->data;
+    hunter->canRead = (EvidenceType)(numHunt);
+    hunter->collect = &(house->foundEvidence);
+    hunter->fear = 0;
+}
+
+void initEvidenceList(EvidenceList* evidenceList){
+    evidenceList->head = NULL;
+    evidenceList->tail = NULL;
+}
+
+void initRoomList(RoomList* roomList){
+    roomList->head = NULL;
+    roomList->tail = NULL;
+}
+
+void initHouse(House** house) {
+    *house = (House*)malloc(sizeof(House));
+
+    initRoomList(&((*house)->totalRoomList));
+    initEvidenceList(&((*house)->foundEvidence));
+    
+    for (int i = 0; i < NUM_HUNTERS; ++i) {
+        initHunter(&((*house)->huntersInRoom[i]), *house, i);
+    }
+}
+
+Room* createRoom(char* name) {
+    //Allocate space
+    Room* room = (Room*)malloc(sizeof(Room));
+    //copy name
+    strcpy(room->name, name);
+    //set pointer of ghost to null and init both lists
+
+    room->ghost = NULL;    
+    initEvidenceList(&(room->ev));
+    initRoomList(&(room->roomList));
+    return room;
+}
+
+void connectRooms(Room* room1, Room* room2){
+    //make space for both rooms
+    RoomNode* node1 = (RoomNode*)malloc(sizeof(RoomNode));
+    RoomNode* node2 = (RoomNode*)malloc(sizeof(RoomNode));
+
+    node1->data = room1;
+    node1->next = NULL;
+
+    node2->data = room2;
+    node2->next = NULL;
+
+
+    //add based to end if list, checking if empty to adjust head or just the tail
+    if(room1->roomlist.head==NULL){
+        room1->roomlist.head=node2;
+        room1->roomlist.tail=node2;
+    }
+    else{
+        room1->roomlist.tail->next = node2;
+        room1->roomlist.tail = node2;
+    }
+
+    if (room2->roomlist.head == NULL) {
+        room2->roomlist.head = node1;
+        room2->roomlist.tail = node1;
+    } 
+    else {
+        room2->roomlist.tail->next = node1;
+        room2->roomlist.tail = node1;
+    }
+
+}
+
+void addRoom(RoomList* roomList, Room* room) {
+    RoomNode* newNode = (RoomNode*)malloc(sizeof(RoomNode));
+    newNode->data = room;
+    newNode->next = NULL;
+
+    if (roomList->head == NULL) {
+        roomList->head = newNode;
+        roomList->tail = newNode;
+    } 
+    else {
+        roomList->tail->next = newNode;
+        roomList->tail = newNode;
+    }
+}
+
 /*
     Dynamically allocates several rooms and populates the provided house.
     Note: You may modify this as long as room names and connections are maintained.
@@ -53,103 +149,4 @@ void populateRooms(HouseType* house) {
     addRoom(&house->rooms, living_room);
     addRoom(&house->rooms, garage);
     addRoom(&house->rooms, utility_room);
-}
-
-Room* createRoom(char* name) {
-    //Allocate space
-    Room* room = (Room*)malloc(sizeof(Room));
-    //copy name
-    strcpy(room->name, name);
-    //set pointer of ghost to null and init both lists
-
-    room->ghost = NULL;    
-    initEvidenceList(&(room->ev));
-    initRoomList(&(room->roomList));
-    return room;
-}
-
-void initHouse(House** house) {
-    *house = (House*)malloc(sizeof(House));
-
-    initRoomList(&((*house)->totalRoomList));
-    initEvidenceList(&((*house)->foundEvidence));
-    
-    for (int i = 0; i < NUM_HUNTERS; ++i) {
-        initHunter(&((*house)->huntersInRoom[i]), *house, i);
-    }
-}
-
-
-void initGhost(Ghost* ghost) {
-    ghost->ghostType = randomGhost();
-    ghost->boredom = 0;
-    ghost->room = NULL;
-}
-
-void initRoomList(RoomList* roomList){
-    roomList->head = NULL;
-    roomList->tail = NULL;
-}
-
-void connectRooms(Room* room1, Room* room2){
-    //make space for both rooms
-    RoomNode* node1 = (RoomNode*)malloc(sizeof(RoomNode));
-    RoomNode* node2 = (RoomNode*)malloc(sizeof(RoomNode));
-
-    node1->data = room1;
-    node1->next = NULL;
-
-    node2->data = room2;
-    node2->next = NULL;
-
-
-    //add based to end if list, checking if empty to adjust head or just the tail
-    if(room1->roomlist.head==NULL){
-        room1->roomlist.head=node2;
-        room1->roomlist.tail=node2;
-    }
-    else{
-        room1->roomlist.tail->next = node2;
-        room1->roomlist.tail = node2;
-    }
-
-    if (room2->roomlist.head == NULL) {
-        room2->roomlist.head = node1;
-        room2->roomlist.tail = node1;
-    } 
-    else {
-        room2->roomlist.tail->next = node1;
-        room2->roomlist.tail = node1;
-    }
-
-}
-
-void addRoom(RoomList* roomList, Room* room) {
-    RoomNode* newNode = (RoomNode*)malloc(sizeof(RoomNode));
-    newNode->data = room;
-    newNode->next = NULL;
-
-    if (roomList->head == NULL) {
-        roomList->head = newNode;
-        roomList->tail = newNode;
-    } 
-    else {
-        roomList->tail->next = newNode;
-        roomList->tail = newNode;
-    }
-}
-
-
-void initEvidenceList(EvidenceList* evidenceList){
-    evidenceList->head = NULL;
-    evidenceList->tail = NULL;
-}
-
-void initHunter(Hunter* hunter, House* house, int numHunt) {
-    printf("This is hunter #%d, what should his name be",numHunt+1);
-    scanf("%s", hunter->name);
-    hunter->roomIn = house->totalRoomList.head->data;
-    hunter->canRead = (EvidenceType)(numHunt);
-    hunter->collect = &(house->foundEvidence);
-    hunter->fear = 0;
 }
